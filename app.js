2eb1853c5bcdd63f82ddc034fb87d94e53938a9d
absmsn <17734085891@163.com>
2020-08-16 11:40:12 +0800

    perf:为静态文件添加gzip压缩

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const compression = require('compression')
 
 const port = process.env.PORT || 4000
 const host = '0.0.0.0'
 
+app.use(compression())
 app.use(express.static("dist"))
 
 app.listen(port, host, ()=>{
